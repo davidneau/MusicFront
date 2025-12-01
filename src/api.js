9bef5ad67d167926073a517d5c538dcbed8126da
david <davidneau49@gmail.com>
2025-12-02 00:22:07 +0100

    change api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://musicback-0vef.onrender.com:5000',
 });
 
 export function login(identifiant, password) {
