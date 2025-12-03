108d6fa9287338c224087bd63518d431a5514ee9
david <davidneau49@gmail.com>
2025-12-03 13:28:49 +0100

    bidoullage

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'http://10.21.237.21:5000',
 });
 
 export function login(identifiant, password) {
