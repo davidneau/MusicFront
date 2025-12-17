dabff73efa44a00251079a90749bce2e3f408401
david <davidneau49@gmail.com>
2025-12-18 00:16:42 +0100

    wip

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
-  //baseURL: 'https://musicback-0vef.onrender.com',
+  //baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://musicback-0vef.onrender.com',
 });
 
 export function login(identifiant, password) {
