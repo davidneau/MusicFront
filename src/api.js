8373231c9e6dc4185a1622d9d2d4df02a398e467
david <davidneau49@gmail.com>
2026-01-02 22:13:06 +0100

    WIP

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   //baseURL: 'http://127.0.0.1:5000',
-  baseURL: 'https://musicda.cloud',
+  baseURL: 'https://musicda.cloud:5000',
 });
 
 export function login(identifiant, password) {
