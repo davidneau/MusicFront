cf34550abd551e064e5c2afe0e28d13f390489ce
david <davidneau49@gmail.com>
2026-01-09 00:49:12 +0100

    wip

---
+++ src/views/SignIn.vue
@@ -0,0 +1,97 @@
+<template>
+    <div>
+        <div id="bannerSearch">
+            <div v-if="userConnected">
+                <input type="text" id="search" @keyup.enter="search" ref="searchInput" value="21 pilots">
+                <button @click="search(true)">Search</button>
+            </div>
+        </div>
+        <div id="divLogin">
+            <h1>Login</h1>
+            <h2>Email</h2>
+            <input type="text" id="identifiant"/>
+            <h2>Mot de passe</h2>
+            <input type="password" id="password1"/>
+            <h2>Confirmation du mot de passe</h2>
+            <input type="password" id="password2" @keypress.enter="enter"/>
+            <p id="error">Les deux mots de passes ne correspondent pas</p>
+            <button @click="enter">Valider</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { signInUser } from '../api';
+
+export default ({
+    name: "LoginPage",
+    methods: {
+        enter(){
+            let identifiant = document.getElementById("identifiant").value
+            let password1 = document.getElementById("password1").value
+            let password2 = document.getElementById("password2").value
+            console.log(identifiant)
+            console.log(password1)
+
+            if(password1 == password2){
+                signInUser(identifiant, password1)
+                .then((response) => {
+                    console.log(response.data)
+                    this.$router.push('/')
+                })
+                .catch(err => {
+                    console.error('Erreur:', err.response);
+                });
+            }
+            else {
+                document.getElementById("error").style.visibility = "visible"
+            }
+        }
+    }
+})
+</script>
+
+<style>
+
+h1, h2 {
+    margin: 0;
+}
+
+#divLogin{
+    margin-left: 25%;
+    width: 50%;
+    margin-top: 200px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #00ebff;
+    border-radius: 15px;
+}
+
+#error{
+    color: red;
+    visibility: hidden;
+}
+
+input{
+    width: 90% !important;
+}
+
+@media screen and (min-width: 428px)  {
+    #divLogin{
+        margin-left: 45%;
+        width: 10%;
+        margin-top: 200px;
+        height: 250px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #00ebff;
+        border-radius: 15px;
+    }
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <div id="bannerSearch">
+            <div v-if="userConnected">
+                <input type="text" id="search" @keyup.enter="search" ref="searchInput" value="21 pilots">
+                <button @click="search(true)">Search</button>
+            </div>
+        </div>
+        <div id="divLogin">
+            <h1>Login</h1>
+            <h2>Id</h2>
+            <input type="text" id="identifiant"/>
+            <h2>Password</h2>
+            <input type="password" id="password" @keypress.enter="enter"/>
+            <button @click="enter">Enter</button>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { login, getProfile } from '../api';
+
+export default ({
+    name: "LoginPage",
+    methods: {
+        enter(){
+            let identifiant = document.getElementById("identifiant").value
+            let password = document.getElementById("password").value
+            console.log(identifiant)
+            console.log(password)
+
+            login(identifiant, password)
+            .then(() => {
+                return getProfile();
+            })
+            .then(profile => {
+                console.log(profile.data); // Affiche les infos de l'utilisateur
+                this.$router.push('home')
+            })
+            .catch(err => {
+                console.error('Erreur:', err.response);
+                alert("mot de passe ou identifiant erroné")
+            });
+        }
+    }
+})
+</script>
+
+<style>
+
+h1, h2 {
+    margin: 0;
+}
+
+#divLogin{
+    margin-left: 25%;
+    width: 50%;
+    margin-top: 200px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #00ebff;
+    border-radius: 15px;
+}
+
+input{
+    width: 90% !important;
+}
+@media screen and (min-width: 428px)  {
+    #divLogin{
+        margin-left: 45%;
+        width: 10%;
+        margin-top: 200px;
+        height: 250px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        background-color: #00ebff;
+        border-radius: 15px;
+    }
+}
+</style>
