15f3b93e629c1885e0b15123b6ffab46790feb4c
david <davidneau49@gmail.com>
2026-01-08 20:40:58 +0100

    WIP

---
+++ src/components/Description.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="mainDescription">
+    <div id="Lyrics"></div>
+  </div>
+</template>
+
+<script>
+import { getLyrics } from '@/api';
+
+export default {
+  name: "DescriptionVideo",
+  data() {
+    return {
+        Title: "",
+        Artist: ""
+    };
+  },
+  methods: {
+    setLyrics() {
+        getLyrics(this.Artist + "(sep)" + this.Title)
+        .then(async (response) => {
+            console.log("Lyrics", response.data)
+            document.getElementById("Lyrics").innerText = response.data
+        })
+    }
+  },
+  mounted() {
+  }
+};
+
+// Fonction globale fournie par YouTube qui est appelée quand l'API est prête
+window.onYouTubeIframeAPIReady = function() {
+  if (window.vueInstance) {
+    window.vueInstance.onYouTubeIframeAPIReady(); // Appelle la méthode de l'instance Vue
+  }
+};
+</script>
+
+<style>
+
+#mainDescription{
+    background-color: white;
+}
+
+</style>
