ba271b899979822264ceaf7b761c8b1ebb14d3bb
david <davidneau49@gmail.com>
2026-05-18 16:19:17 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   transpileDependencies: true,
   pwa: {
-    workboxPluginMode: "GenerateSW", // ou "InjectManifest"
+    workboxPluginMode: "InjectManifest", // ou "InjectManifest"
     workboxOptions: {
       globPatterns: ["**/*.{html,js,css,png,svg}"],
       globIgnores: ["**/_redirects"] // 👈 ICI,
