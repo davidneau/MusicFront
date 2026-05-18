aa707e60f8f241cc321f1a9ff2df90debcbab007
david <davidneau49@gmail.com>
2026-05-18 15:56:17 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
   pwa: {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: './src/sw.js', // Utilise votre propre fichier comme source
+      swSrc: './public/sw.js', // Utilise votre propre fichier comme source
     },
   },
 };
