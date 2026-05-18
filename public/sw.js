bef943253808eb4c61266dfedb6fba38b96336ec
david <davidneau49@gmail.com>
2026-05-18 16:30:56 +0200

    fix

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,44 @@
-/* eslint-disable no-undef */
-import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
+const CACHE_NAME = "app-cache-v1";
 
-self.skipWaiting()
-clientsClaim()
-cleanupOutdatedCaches()
+const STATIC_ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
 
-precacheAndRoute(self.__WB_MANIFEST)
+// Installation
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting()
-  }
-})
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+// Activation
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// Fetch (offline support simple)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
