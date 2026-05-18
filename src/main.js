669a3fbd087970ed84b8c42d45b03a6400afd91b
david <davidneau49@gmail.com>
2026-05-18 15:46:42 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.js'
+import router from './router'
+import './registerServiceWorker'
 
-const app = createApp(App)
-
-app.use(router)
-app.mount('#app')
+createApp(App).use(router).mount('#app')
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
@@ -14,5 +12,3 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.error('Erreur Service Worker :', err))
   })
 }
-
-export default app
