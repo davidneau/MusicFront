8424c01db344bfcf258f7421693f56531602ba09
david <davidneau49@gmail.com>
2026-05-19 12:59:40 +0200

    bug

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import './registerServiceWorker'
 
 createApp(App).use(router).mount('#app')
 
-/* if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
       .then(reg => console.log('Service Worker enregistré :', reg))
       .catch(err => console.error('Erreur Service Worker :', err))
   })
-} */
+}
