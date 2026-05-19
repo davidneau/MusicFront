1feb891c535e777060154e16483a8bc4e3684b2f
david <davidneau49@gmail.com>
2026-05-19 13:08:31 +0200

    bug

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,4 @@
 module.exports = {
   publicPath: '/',
-  pwa: {
-    name: 'MusicDA',
-    themeColor: '#111111',
-    msTileColor: '#111111',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    workboxPluginMode: 'GenerateSW',
-    workboxOptions: {
-      skipWaiting: true,
-      clientsClaim: true,
-      cleanupOutdatedCaches: true,
-      exclude: [
-        /_headers$/,
-        /_redirects$/,
-        /\.map$/
-      ]
-    }
-  }
+  pwa: false
 }
