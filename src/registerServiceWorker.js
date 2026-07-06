2434432d10f3fef5d9ce89ca7eb14d5a46e67a59
davidneau <38453079+davidneau@users.noreply.github.com>
2026-07-06 07:54:11 +0000

    enleve pause

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,7 +1,5 @@
 import { register } from 'register-service-worker'
 
-let refreshing = false
-
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}sw.js`, {
     ready() {
